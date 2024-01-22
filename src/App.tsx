3f293fc93f1d5ee2b02127aa83e34e5ae7c2c0bc
Naibuu <hawkwer141099@gmail.com>
2024-01-22 19:56:45 +0200

    add tips

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,12 @@ export default function App() {
 
     const canvasRef = useRef<HTMLCanvasElement | null>(null)
     const brandRef = useRef<HTMLImageElement | null>(null)
+    const debugRef = useRef(false)
 
     useEffect(() => {
         const canvas = canvasRef.current
         const brand = brandRef.current
+        debugRef.current = false
 
         if (!canvas || !brand) return
         const ctx = canvas.getContext('2d')!
@@ -30,6 +32,14 @@ export default function App() {
         }
 
         window.addEventListener('resize', updateCanvasSize)
+
+        window.addEventListener('keydown', (e: any) => {
+            if (e.code === 'KeyE') {
+                debugRef.current = !debugRef.current
+            } else if (e.code === 'KeyD') {
+                window.open('https://discord.gg/x8BHPftbar', '_blank')
+            }
+        })
 
         const animate = () => {
             ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -53,48 +63,58 @@ export default function App() {
 
             ctx.globalCompositeOperation = 'destination-over'
 
-            const centerX = canvas.width / 2
-            const centerY = canvas.height / 2
-            const lineX = x + brand.width / 1.5 / 2
-            const lineY = y + brand.height / 1.5 / 2
-            const angle = Math.atan2(vy, vx)
-            const endX = lineX + 300 * Math.cos(angle)
-            const endY = lineY + 300 * Math.sin(angle)
+            if (debugRef.current) {
+                const lineX = x + brand.width / 1.5 / 2
+                const lineY = y + brand.height / 1.5 / 2
 
-            /**
-             * Debug Line (Red: Center)
-             */
-            ctx.strokeStyle = 'red'
-            ctx.beginPath()
-            ctx.moveTo(centerX, centerY)
-            ctx.lineTo(lineX, lineY)
-            ctx.stroke()
+                /**
+                 * Debug Line (Red: Center)
+                 */
+                ctx.strokeStyle = 'red'
+                ctx.beginPath()
+                ctx.moveTo(canvas.width / 2, canvas.height / 2)
+                ctx.lineTo(lineX, lineY)
+                ctx.stroke()
 
-            /**
-             * Debug Line (Green: Facing direction)
-             */
-            ctx.strokeStyle = 'green'
-            ctx.beginPath()
-            ctx.moveTo(lineX, lineY)
-            ctx.lineTo(endX, endY)
-            ctx.stroke()
+                /**
+                 * Debug Line (Green: Facing direction)
+                 */
+                const angle = Math.atan2(vy, vx)
+                const endX = lineX + 300 * Math.cos(angle)
+                const endY = lineY + 300 * Math.sin(angle)
 
-            /**
-             * Debug Text
-             */
-            const textX = Math.min(Math.max(lineX - 25, 0), canvas.width - 120)
-            const textY = Math.min(
-                Math.max(lineY + brand.height / 1.5 / 2 + 20, 0),
-                canvas.height - 80
-            )
+                ctx.strokeStyle = 'green'
+                ctx.beginPath()
+                ctx.moveTo(lineX, lineY)
+                ctx.lineTo(endX, endY)
+                ctx.stroke()
+
+                /**
+                 * Debug Text
+                 */
+                const textX = Math.min(
+                    Math.max(lineX - 25, 0),
+                    canvas.width - 120
+                )
+                const textY = Math.min(
+                    Math.max(lineY + brand.height / 1.5 / 2 + 20, 0),
+                    canvas.height - 80
+                )
+
+                ctx.fillStyle = 'white'
+                ctx.font = '14px Rubik'
+                ctx.textAlign = 'left'
+                ctx.fillText(`x: ${x.toFixed(2)}`, textX, textY)
+                ctx.fillText(`y: ${y.toFixed(2)}`, textX, textY + 20)
+                ctx.fillText(`vx: ${vx.toFixed(2)}`, textX, textY + 40)
+                ctx.fillText(`vy: ${vy.toFixed(2)}`, textX, textY + 60)
+            }
 
             ctx.fillStyle = 'white'
             ctx.font = '14px Rubik'
             ctx.textAlign = 'left'
-            ctx.fillText(`x: ${x.toFixed(2)}`, textX, textY)
-            ctx.fillText(`y: ${y.toFixed(2)}`, textX, textY + 20)
-            ctx.fillText(`vx: ${vx.toFixed(2)}`, textX, textY + 40)
-            ctx.fillText(`vy: ${vy.toFixed(2)}`, textX, textY + 60)
+            ctx.fillText(`[D] Discord`, 5, 15)
+            ctx.fillText(`[E] Debug`, 5, 35)
 
             x += vx
             y += vy
